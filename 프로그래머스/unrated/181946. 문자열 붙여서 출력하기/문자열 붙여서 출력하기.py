b74a32cadb4a469d4ca62135ea96b761e324f0d1
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-10 01:17:53 +0900

    ref : 문자열 붙여서 출력하기.py

--- 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.py
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.py
@@ -2,3 +2,6 @@ str1, str2 = input().strip().split(' ')
 if 1 <= len(str1) <= 10 and 1 <= len(str2) <= 10:
     output = str1 + str2
     print(output)
+
+# 이렇게 공백을 줄이면 한 줄로 끝남
+print(input().strip().replace(' ', ''))
