ddf5f99b32518d816afea8e9cb84fb7278946c80
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-10 01:36:03 +0900

    ref: 홀짝 구분하기.py

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.py
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -1,3 +1,7 @@
 a = int(input())
-
 print(f"{a} is even" if 1 <= a <= 1000 and a % 2 == 0 else f"{a} is odd")
+
+
+# 문자열 포맷팅을 통해서 짝수면 even 홀수면 odd를 출력
+n = int(input())
+print(f"{n} is {'even' if n % 2 == 0 else 'odd'}")
