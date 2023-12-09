f78684dfa883320d35c5412bfc4c62969d0eac8c
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-10 02:07:27 +0900

    ref: 문자열 겹쳐쓰기.py

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -9,3 +9,6 @@ def solution(my_string, overwrite_string, s):
             answer += my_string[i]
             i += 1
     return answer
+
+# 나는 조건문, 반복문 썼는데 안쓰고 간단하게 하는 방법
+answer = my_string[:s] + overwrite_string + my_string[ s + len(overwrite_string): ]
