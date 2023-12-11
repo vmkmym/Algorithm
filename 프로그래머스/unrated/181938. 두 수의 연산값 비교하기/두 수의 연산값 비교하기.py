71107d1b6a775d41a660b96eee3a125cb3b82bce
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-11 22:11:30 +0900

    ref: 두 수의 연산값 비교하기.py

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -2,3 +2,6 @@ def solution(a, b):
     ans = int(f"{a}{b}")
     wer = int(f"{2 * a * b}")
     return ans if ans == wer else max(ans, wer)
+
+# ans if ans == wer 없이도 한 줄로 끝나네,,
+return max(int(str(a) + str(b)), 2 * a * b)
