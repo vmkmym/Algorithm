9660c1b04be8b7261f9c1f50b680d899b1e058da
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-11 21:37:04 +0900

    ref: 문자열 섞기.py

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.py
@@ -1,3 +1,8 @@
 def solution(str1, str2):
     answer = ''.join(i + j for i, j in zip(str1, str2))
     return answer
+
+# 다른 사람의 풀이
+def solution(str1, str2):
+    answer = ''.join([str1[i] + str2[i] for i in range(len(str1))])
+    return answer
