a10365a5ebc4406e43899e358df82581dcb812ee
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-13 03:59:37 +0900

    ref: 공배수.py

--- 프로그래머스/unrated/181936. 공배수/공배수.py
+++ 프로그래머스/unrated/181936. 공배수/공배수.py
@@ -1,2 +1,8 @@
 def solution(num, n, m):
     return int((num % n == 0) and (num % m == 0))
+
+# 입력값 조건 추가 시
+def solution(num, n, m):
+    if not (2 <= n < 10 and 2 <= m < 10 and 10 <= num <= 100):
+        return 0
+    return int((num % n == 0) and (num % m == 0))
