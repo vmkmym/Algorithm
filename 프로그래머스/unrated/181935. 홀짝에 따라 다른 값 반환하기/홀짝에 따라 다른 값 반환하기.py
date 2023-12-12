155dcd48a74a80688bc754129b50159c0e8d7239
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-13 04:28:50 +0900

    ref: 홀짝에 따라 다른 값 반환하기.py

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -9,3 +9,13 @@ def solution(n):
             if i % 2 == 0:
                 answer += i * i
     return answer  
+
+# 다른 고수의 풀이, 이해가 잘 안감
+def solution(n):
+    return sum(x ** (2 - x % 2) for x in range(n + 1) if n % 2 == x % 2)
+
+# 이거는 이해가 감 range(시작값, 마지막값, 간격)
+def solution(n):
+    if n%2:
+        return sum(range(1,n+1,2))
+    return sum([i*i for i in range(2,n+1,2)])
