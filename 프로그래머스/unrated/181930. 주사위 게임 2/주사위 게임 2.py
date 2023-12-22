86bef1cf0b3b96d7aecae0b907f5ee131601fec6
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-22 22:15:33 +0900

    ref: 주사위 게임 2.py

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -8,3 +8,5 @@ def solution(a, b, c):
         return (a + b + c) * (a**2 + b**2 + c**2)
     else:
         return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+
+# 다른 사람의 풀이가 괜찮은게 딱히 없었음
