204b3ce27a1ac754a5e3fe4d57338c8aa3a462de
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-22 21:02:33 +0900

    ref: 코드 처리하기.py

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -9,3 +9,13 @@ def solution(code):
             mode = 1 - mode
 
     return ''.join(ret) if ret else "EMPTY"
+
+# 다른 사람 풀이
+def solution(code):
+    return "".join(code.split("1"))[::2] or "EMPTY"
+
+# code.split("1"): 입력된 문자열을 '1'을 기준으로 분할하여 리스트로 변환
+# "".join(code.split("1"))[::2]: 분할된 문자열 리스트에서 짝수 번째 문자열을 가져와 이어붙임
+# or "EMPTY": 위에서 만든 문자열이 비어있다면("".join(code.split("1"))[::2]가 False인 경우), "EMPTY"를 반환
+
+
