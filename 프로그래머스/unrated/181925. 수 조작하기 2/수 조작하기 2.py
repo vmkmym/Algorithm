0e2b94abe2bb7e7d3c81bb3990480058aa1ede2b
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-24 02:57:31 +0900

    ref: 수 조작하기 2.py

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,4 +1,20 @@
 def solution(numLog):
+    result = ''
+    log = {1: 'w', -1: 's', 10: 'd', -10: 'a'}
+
+    for i in range(1, len(numLog)): 
+        diff = numLog[i] - numLog[i - 1]
+        result += log[diff] if diff in log else ''
+
+    return result
+
+# 다른 풀이
+def solution(numLog):
+    diff = numLog[i] - numLog[i - 1]
+    return "".join(["w" if diff == 1 else "s" if diff == -1 else "d" if diff == 10 else "a" for i in range(1, len(numLog))])
+
+# 또 다른 풀이 : 근데 너무 효율 안좋음...
+def solution(numLog):
     control = []
     
     for i in range(len(numLog)):
