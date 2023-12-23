957ebfaec0ca7a2f9a9e718eea7b386e51d6ddaa
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-24 02:05:59 +0900

    ref: 수 조작하기 1.py

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -9,3 +9,10 @@ def solution(n, control):
         elif i == 'a':
             n -= 10
     return n
+
+# 5점받았지만 더 짧게 쓰고 싶었음
+
+# 딕셔너리, 리스트 컴프리헨션 사용하는 풀이
+def solution(n, control):
+    c = {'w': 1, 's': -1, 'd': 10, 'a': -10}
+    return n + sum(c[i] for i in control)
