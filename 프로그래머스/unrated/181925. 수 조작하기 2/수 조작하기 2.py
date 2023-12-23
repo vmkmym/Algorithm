59b0ef427a1154df75b5c9e3844ef901a6801df8
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-24 02:59:00 +0900

    [level unrated] Title: 수 조작하기 2, Time: 14.00 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -7,26 +7,3 @@ def solution(numLog):
         result += log[diff] if diff in log else ''
 
     return result
-
-# 다른 풀이
-def solution(numLog):
-    diff = numLog[i] - numLog[i - 1]
-    return "".join(["w" if diff == 1 else "s" if diff == -1 else "d" if diff == 10 else "a" for i in range(1, len(numLog))])
-
-# 또 다른 풀이 : 근데 너무 효율 안좋음...
-def solution(numLog):
-    control = []
-    
-    for i in range(len(numLog)):
-        diff = numLog[i] - (numLog[i - 1] if i > 0 else 0)
-
-        if diff == 1:
-            control.append('w')
-        elif diff == -1:
-            control.append('s')
-        elif diff == 10:
-            control.append('d')
-        elif diff == -10:
-            control.append('a')
-
-    return ''.join(control)
