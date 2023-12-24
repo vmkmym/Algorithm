77bfc4d603ecbeb535d9a4249f71257640124107
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-24 23:47:22 +0900

    ref: 수열과 구간 쿼리 2.py

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,9 +1,12 @@
 def solution(arr, queries):
     result = []
     for s, e, k in queries:
-        value = [x for x in arr[s:e+1] if x > k]
+        value = [i for i in arr[s:e+1] if i > k]
         if value:
             result.append(min(value))
         else:
             result.append(-1)
     return result
+
+# s, e, k로 쓰고 싶었음
+# value에 리스트 컴프리헨션 이용하니까 코드가 간결해진다
