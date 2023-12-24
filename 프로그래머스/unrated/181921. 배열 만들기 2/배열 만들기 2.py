a1749fc49c2c71dbbf66e89f0b46f613499ef6d5
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-25 00:47:00 +0900

    ref: 배열 만들기 2.py

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -3,3 +3,7 @@ def solution(l, r):
     
     # sorted()에서 정렬의 기준을 지정하는 방법
     return sorted(result, key=lambda x: int(x))
+
+# 시간 무슨 일이지.. 근데 점수는 높다..
+# 파이썬에서 sorted 사용할 때 정렬 기준을 람다식으로 지정한다는 것을 처음 알게 됨
+# set은 중복값 허용x, 순서x, 변경불가능 컬렉션으로 {}를 쓰고 쉼표로 구분
