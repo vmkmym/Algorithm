0d90037e141ed1eba013b66e49160ce67199987d
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-25 20:24:43 +0900

    ref: 배열 만들기 4.py

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
@@ -13,3 +13,17 @@ def solution(arr):
             stk.pop()
     
     return stk
+
+# 음 뭐지 왜 5점이지?
+
+# gpt 풀이, 다른 사람 풀이도 이거랑 비슷함
+# 입력 배열을 한 번 순회하는 선형 시간복잡도를 갖고 있으므로 입력 크기에 대해 효율적임
+def solution(arr):
+    stk = []
+    
+    for num in arr:
+        while stk and stk[-1] <= num:
+            stk.pop()
+        stk.append(num)
+    
+    return stk
