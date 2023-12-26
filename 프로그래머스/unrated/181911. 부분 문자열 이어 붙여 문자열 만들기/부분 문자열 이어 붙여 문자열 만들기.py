9d04f13f5cff7d43787c948f5ee99e66bb389640
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-27 01:48:36 +0900

    ref: 문자열 배열, 2차원 정수 배열, zip, join

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -6,6 +6,6 @@ def solution(my_strings, parts):
         result.append(my_str[s:e + 1])
 
     return ''.join(result)
-
-        
-            
+    
+# 다른 사람 풀이는 대체로 비슷하고 지금 이 코드를 한 줄 코딩한 풀이가 많았음
+# zip을 활용할 수 있는 좋은 예제
