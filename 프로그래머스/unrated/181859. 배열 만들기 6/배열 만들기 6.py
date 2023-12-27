af04cfe095c5322e8cb7e02cbab02feb98bf41cb
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-28 02:07:17 +0900

    ref: return obj1 or obj2

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -11,3 +11,23 @@ def solution(arr):
 
     return stk if stk else [-1]
 
+# return stk or [-1] 로도 작성할 수 있음. 
+# 객체1 or 객체2 연산은 bool(객체1)이 True이면 객체1을, False이면(비어 있으면) 객체2를 반환
+
+# 다른 풀이
+def solution(arr):
+    stk = []
+    i = 0
+    
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] == arr[i]:
+            stk.pop()
+            i += 1
+        else:
+            stk.append(arr[i])
+            i += 1
+            
+    return stk if len(stk) > 0 else [-1]
