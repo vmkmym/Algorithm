f55861517ba76608d4a57e403609201553864fef
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-28 01:30:45 +0900

    ref: s1, e1 = query[0] 익숙해지자

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,3 +1,8 @@
 def solution(arr, intervals):
     return arr[intervals[0][0]:intervals[0][1] + 1] + arr[intervals[1][0]:intervals[1][1] + 1]
 
+# 다른 사람 풀이
+def solution(arr, intervals):
+    s1, e1 = intervals[0]
+    s2, e2 = intervals[1]
+    return arr[s1:e1+1] + arr[s2:e2+1]
