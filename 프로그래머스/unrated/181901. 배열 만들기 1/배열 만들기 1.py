e79cb41a4b4084aee37e9190d0a99c6bd15af17a
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-28 01:23:02 +0900

    ref: for i in range(start, end, sep)

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,2 +1,7 @@
 def solution(n, k):
     return [i for i in range(1, n + 1) if i % k == 0]
+
+# 다른 사람 풀이
+# 배수라서 1부터 안해도 되고 k부터, k간격으로, n+1까지
+def solution(n, k):
+    return [i for i in range(k,n+1,k)]
