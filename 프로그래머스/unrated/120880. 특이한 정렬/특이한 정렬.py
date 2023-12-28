94c432a047cc2bec2aaca7fcf435035c19c5f1f3
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-28 22:26:01 +0900

    ref: sorted(), key 매개변수로 정렬 기준 지정하기

--- 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.py
@@ -1,2 +1,5 @@
 def solution(numlist, n):
     return sorted(numlist, key = lambda i: (abs(i - n), -i))
+
+# 정수 리스트를 받아서 n과의 거리 비교 : 각 요소에서 n 뺀 절댓값, 거리가 같은 경우 음의 값으로 크기 비교 후 정렬
+[sorted(), key 매개변수 : 정렬 기준](https://github.com/vmkmym/Algorithm/tree/main/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4/unrated/181921.%E2%80%85%EB%B0%B0%EC%97%B4%E2%80%85%EB%A7%8C%EB%93%A4%EA%B8%B0%E2%80%852)
