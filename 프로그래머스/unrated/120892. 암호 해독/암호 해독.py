c33d23834d2f94c9df391f363d217b61b9ee3745
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-30 01:29:43 +0900

    ref: 특정 인덱스만 추출하기

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
@@ -1,8 +1,10 @@
 def solution(cipher, code):
     key = ""
     
-    for i in range(1, len(cipher) +1):
+    for i in range(1, len(cipher)+1):
         if i % code == 0:
             key += cipher[i-1]
             
     return key
+
+# 다른 풀이 아직 추가 안함.. 
