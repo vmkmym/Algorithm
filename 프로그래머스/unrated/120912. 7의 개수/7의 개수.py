023c2187b9bc676e360ef8d3137bac4b524d3098
상은 <71699054+vmkmym@users.noreply.github.com>
2023-12-29 21:59:52 +0900

    ref: 요소를 카운트하기

--- 프로그래머스/unrated/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.py
@@ -1,2 +1,9 @@
 def solution(array):
     return sum(str(i).count('7') for i in array)
+
+# 다른 사람 풀이
+def solution(array):
+    return str(array).count('7')
+
+def solution(array):
+    return ''.join(map(str, array)).count('7')
