9e8457495f292561a510096de9921f8062aee9be
SangEun <71699054+vmkmym@users.noreply.github.com>
2023-12-31 23:27:20 +0900

    ref: list comprehension

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.py
@@ -7,4 +7,7 @@ def solution(cipher, code):
             
     return key
 
-# 다른 풀이 아직 추가 안함.. 
+# 다른 풀이
+def solution(cipher, code):
+    key = ''.join(cipher[i-1] for i in range(1, len(cipher)+1) if i % code == 0)
+    return key
