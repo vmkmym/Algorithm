2413947a9f73436caca39fa218c3a6b90a3b54c2
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-01 19:52:53 +0900

    ref: max, min, indexing

--- 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,2 +1,15 @@
 def solution(dots):
     return (max(dots)[0]-min(dots)[0]) * (max(dots)[1]-min(dots)[1])
+
+# 뭐지 다른 사람 풀이 맨 위가 내 풀이라니!
+# return max(dots)[0]-min(dots)[0] * max(dots)[1]-min(dots)[1]
+    # 처음에 이렇게 풀었는데 안되서 뭐가 문제지 하다가 괄호하니까 통과
+
+# 다른 사람 풀이
+def solution(dots):
+    dots.sort()
+
+    x_min, y_min = dots[0]
+    x_max, y_max = dots[-1]
+
+    return (x_max-x_min) * (y_max-y_min)
