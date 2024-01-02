dd41126067eea6357ee072f43a6fe063e670b4b7
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-02 19:30:32 +0900

    ref: 숏코딩 폼 미쳐따

--- 프로그래머스/unrated/120829. 각도기/각도기.py
+++ 프로그래머스/unrated/120829. 각도기/각도기.py
@@ -8,4 +8,15 @@ def solution(angle):
     else:
         return 4
 
-    
+# 다른 사람 풀이 : 레전드
+# 이런 생각은 어떻게 하는거지
+def solution(angle):
+    answer = (angle // 90) * 2 + (angle % 90 > 0) * 1
+    return answer
+
+# 다른 사람 풀이
+def solution(angle):
+    if angle<=90:
+        return 1 if angle<90 else 2
+    else:
+        return 3 if angle<180 else 4
