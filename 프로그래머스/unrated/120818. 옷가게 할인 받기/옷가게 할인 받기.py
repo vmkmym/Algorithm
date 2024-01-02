2c999de5a91f33acb34cebefda710cea5606af16
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-03 01:25:41 +0900

    ref: 딕셔너리 활용

--- 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -9,3 +9,10 @@ def solution(price):
         price *= 1
 
     return int(price)
+
+# 다른 사람 풀이
+def solution(price):
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
