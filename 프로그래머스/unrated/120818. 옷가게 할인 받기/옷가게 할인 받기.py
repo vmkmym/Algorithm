0597eaa446ec1f4ab8dfa3932d1d23d47e432f7a
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-03 01:24:26 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,11 @@
+def solution(price):
+    if price >= 500000:
+        price *= 0.8
+    elif price >= 300000:
+        price *= 0.9
+    elif price >= 100000:
+        price *= 0.95
+    else:
+        price *= 1
+
+    return int(price)
