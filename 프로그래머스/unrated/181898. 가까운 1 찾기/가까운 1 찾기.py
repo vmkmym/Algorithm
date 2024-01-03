90f8508cd30b2af7dc1d3c6a67fa150dd229ae01
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-04 01:13:32 +0900

    ref: list_name.index(element, start, end)

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -5,3 +5,24 @@ def solution(arr, idx):
     for i in range(idx, len(arr)):
         if arr[i] == 1:
             return i
+
+# 다른 사람 풀이
+'''
+index() 함수는 리스트(List)나 튜플(Tuple)에서 특정 요소의 인덱스를 찾는 데 사용됩니다. 
+이 함수는 해당 요소의 첫 번째 등장하는 인덱스를 반환합니다.
+
+list_name.index(element, start, end)
+
+list_name: 인덱스를 찾을 리스트 이름
+element: 리스트에서 찾을 요소
+start (옵션): 찾기 시작할 인덱스(default: 0)
+end (옵션): 찾을 범위의 끝 인덱스(default: 리스트의 끝)
+'''
+def solution(arr, idx):
+    answer = 0
+    try:
+        answer = arr.index(1, idx)
+    except:
+        answer = -1
+
+    return answer
