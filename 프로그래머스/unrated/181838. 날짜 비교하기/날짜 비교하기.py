976dfb5327433769bcc56df134532ef7b7cd6e23
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-04 21:20:01 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -3,3 +3,7 @@ def solution(date1, date2):
     day2 = date2[0] * 365 + date2[1] * 30 + date2[2]
 
     return 1 if day1 < day2 else 0
+
+# 다른 사람 풀이
+def solution(date1, date2):
+    return int(date1 < date2)
