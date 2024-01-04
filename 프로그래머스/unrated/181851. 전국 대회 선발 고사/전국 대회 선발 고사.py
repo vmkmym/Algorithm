76d214f535ad66764a13501b7ed9519b3faf9a9d
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-05 03:22:31 +0900

    [level unrated] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,11 +1,3 @@
 def solution(rank, attendance):
-    students = [] 
-
-    for i, rank_value in enumerate(rank):
-        if attendance[i]:
-            students.append((rank_value, i))
-
-    students.sort()
-    result = students[0][1] * 10000 + students[1][1] * 100 + students[2][1]
-    
-    return result
+    arr = sorted([(x, i) for i, x in enumerate(rank) if attendance[i]])
+    return arr[0][1] * 10000 + arr[1][1] * 100 + arr[2][1]
