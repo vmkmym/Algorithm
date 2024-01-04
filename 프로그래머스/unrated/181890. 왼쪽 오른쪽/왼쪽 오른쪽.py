3482a5b4a2d980bfdc546eee06b3f77cfd8a220e
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-05 01:26:10 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,15 +1,7 @@
 def solution(str_list):
-    left_index = -1 if 'l' not in str_list else str_list.index('l')
-    right_index = -1 if 'r' not in str_list else str_list.index('r')
-
-    if left_index >= 0 and right_index >= 0:
-        if left_index < right_index:
-            return str_list[:left_index]
-        else:
-            return str_list[right_index + 1:]
-    elif left_index >= 0:
-        return str_list[:left_index]
-    elif right_index >= 0:
-        return str_list[right_index + 1:]
-    else:
-        return []
+    for i in range(len(str_list)):
+        if str_list[i]=='l': 
+            return str_list[:i]
+        elif str_list[i]=='r': 
+            return str_list[i+1:]
+    return []
