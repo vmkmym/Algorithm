f52b47124c6ea7110c4207e5b2e434c2e6d79d8f
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-06 21:02:08 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -3,5 +3,5 @@ from collections import OrderedDict
 def solution(my_string):
     return ''.join(OrderedDict.fromkeys(my_string))
 
-# 입력된 순서를 유지하면서 중복을 제거하는 collections의 OrderedDict
+# 입력된 순서를 유지하면서 중복을 제거하는 collections의 OrderedDict.fromkeys
 
