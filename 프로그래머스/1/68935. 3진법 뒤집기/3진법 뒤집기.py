1c2bc87d00bbba72d90d7cfda373ca94f0b7e992
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-09 03:47:49 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,13 +1,8 @@
 def solution(n):
     ternary = ''
-    while n > 0:
-        n, remainder = divmod(n, 3)
-        ternary += str(remainder)
     
-    decimal = 0
-    for idx, digit in enumerate(ternary[::-1]):
-        decimal += int(digit) * (3 ** idx)
-    
-    return decimal
+    while n:
+        ternary += str(n % 3)
+        n = n // 3
 
-# gpt 복붙.. 하지만 3진법 이해가 안간다..
+    return int(ternary, 3)
