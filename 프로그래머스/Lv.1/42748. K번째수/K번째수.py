69a9122096f9ca72c34483304c18f885119e0be2
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-12 05:52:46 +0900

    [level Lv.1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/42748. K번째수/K번째수.py
+++ 프로그래머스/Lv.1/42748. K번째수/K번째수.py
@@ -1,8 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for cmd in commands:
-        i, j, k = cmd
-        arr = array[i-1:j]
-        arr.sort()
-        answer.append(arr[k-1])
-    return answer
+    return [sorted(array[i-1:j])[k-1] for i, j, k in commands]
