f41ff3442c0a7879576263cda47029436b7c63bc
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-14 19:43:16 +0900

    [level Lv.1] Title: 2016년, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12901. 2016년/2016년.py
+++ 프로그래머스/Lv.1/12901. 2016년/2016년.py
@@ -1,4 +1,3 @@
-def solution(month, day):
-    days = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
-    months = [31,29,31,30,31,30,31,31,30,31,30,31]
-    return days[(sum(months[:month-1])+day-1)%7]
+def solution(a, b):
+    answer = ''
+    return answer
