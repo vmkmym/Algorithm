741551b8eccc2e5ab0d969680ed26072ba4f5b15
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-14 20:01:52 +0900

    ref: 귤 고르기

--- 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.py
@@ -1,10 +1,13 @@
 from collections import Counter
+
 def solution(k, tangerine):
     dic = Counter(tangerine)
     dic = sorted(dic.values(), reverse=True)
     r = 0
+    
     for i, v in enumerate(dic, 1):
         r += v
         if r >= k:
             return i
+        
     return len(dic)
