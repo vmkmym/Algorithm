904caaac8ca5e397d2d4ce2d2af7d684dce4df7d
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-15 03:17:57 +0900

    ref: 배열이 반복된다면 itertools-cycle

--- 프로그래머스/Lv.1/42840. 모의고사/모의고사.py
+++ 프로그래머스/Lv.1/42840. 모의고사/모의고사.py
@@ -18,3 +18,21 @@ def solution(answers):
             correct_person.append(i + 1)
     correct_person.sort()
     return correct_person
+
+# 다른 사람 풀이
+from itertools import cycle # 이건 처음봄... cycle이라는 함수가 있었구나
+
+def solution(answers):
+    giveups = [
+        cycle([1,2,3,4,5]),
+        cycle([2,1,2,3,2,4,2,5]),
+        cycle([3,3,1,1,2,2,4,4,5,5]),
+    ]
+    scores = [0, 0, 0]
+    for num in answers:
+        for i in range(3):
+            if next(giveups[i]) == num:
+                scores[i] += 1
+    highest = max(scores)
+
+    return [i + 1 for i, v in enumerate(scores) if v == highest]
