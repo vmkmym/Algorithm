5b77bdf592f404e0e8091c89b737ae673fb35adf
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-14 15:47:55 +0900

    ref: 정렬, enumerate 메서드 start 사용해서 idx, value 반환

--- 프로그래머스/Lv.2/42747. H－Index/H－Index.py
+++ 프로그래머스/Lv.2/42747. H－Index/H－Index.py
@@ -5,3 +5,13 @@ def solution(citations):
         if h <= len([i for i in citations if i >= h]):
             h_index = h
     return h_index
+
+
+# 다른 사람 풀이
+def solution(citations):
+    citations.sort(reverse=True)
+    answer = max(map(min, enumerate(citations, start=1))) # enumerate: 인덱스와 값을 동시에 반환
+    return answer
+
+# [6, 5, 3, 1, 0] -> [(1, 6), (2, 5), (3, 3), (4, 1), (5, 0)] 
+# -> [(1, 6), (2, 5), (3, 3), (4, 1), (5, 0)] -> [1, 2, 3, 1, 0] -> 3
