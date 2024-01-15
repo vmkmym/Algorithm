14ce6b5bc3985f0e2ecf76f217f951a0aab66154
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-16 04:17:07 +0900

    ref: ZeroDivisionError, O(N^2) -> O(N log N)
    - ZeroDivisionError는 스테이지에 도달한 플레이어 수가 0인 경우, 즉 아무도 해당 스테이지에 도달하지 않은 경우에 발생합니다.
    
    - 이 경우 실패율을 계산할 때 분모가 0이 되므로 ZeroDivisionError가 발생하게 됩니다.
    
    - 따라서 이를 처리하기 위해 try-except 구문을 사용하여 ZeroDivisionError가 발생하면 실패율을 0으로 설정합니다.
    
    - 이는 문제에서 "스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0으로 정의한다"는 조건을 충족시키기 위함입니다.

--- 프로그래머스/Lv.1/42889. 실패율/실패율.py
+++ 프로그래머스/Lv.1/42889. 실패율/실패율.py
@@ -12,3 +12,23 @@ def solution(N, stages):
     failure_rate.sort(key=lambda x: (-x[1], x[0])) # 실패율 내림차순 정렬 후 같다면 스테이지 번호 오름차순 정렬
 
     return [x[0] for x in failure_rate]
+
+# 이건 시간복잡도 O(N^2)이라서 효율성 테스트가 있었으면 시간초과가 뜰 것 같음.
+# 개선한 코드는 시간 복잡도는 O(N log N), 제출 코드보다 훨씬 효율적임.
+def solution(N, stages):
+    player_failure = [0] * (N + 2) # 0 ~ N + 1
+    for stage in stages: # stage 1 ~ N
+        player_failure[stage] += 1  # stage_count[1] ~ stage_count[N]
+
+    player_success = len(stages) 
+    failure_rate = []
+    for i in range(1, N + 1):
+        if player_success == 0:
+            failure_rate.append((i, 0))
+        else:
+            failure_rate.append((i, player_failure[i] / player_success))
+            player_success -= player_failure[i]
+
+    failure_rate.sort(key=lambda x: (-x[1], x[0]))
+
+    return [x[0] for x in failure_rate]
