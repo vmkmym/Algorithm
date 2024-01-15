9f1f1538a6224339c1d743070859bd60e3d72174
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-16 01:34:53 +0900

    [level Lv.1] Title: 덧칠하기, Time: 1.71 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/Lv.1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/Lv.1/161989. 덧칠하기/덧칠하기.py
@@ -1,10 +1,10 @@
 def solution(n, m, section):
-    paint_count = 0
-    max_val = 0
-    
-    for i in section:
-        if i > max_val:
+    paint_count = 1 # 최소한 한번은 칠해야 하므로 1로 초기화
+    start = section[0] # 칠할 수 있는 범위의 시작점을 초기화
+
+    for i in section: 
+        if i - start >= m: # 칠할 수 있는 범위를 넘어가면
+            start = i # 시작점을 현재 위치로 초기화
             paint_count += 1
-            max_val = i + m - 1
-            
+
     return paint_count
