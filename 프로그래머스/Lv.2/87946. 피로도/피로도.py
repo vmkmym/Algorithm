115ddc339746be35a00b8e20b966c64a8c1ed293
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-15 19:24:50 +0900

    ref: 완전탐색 문제(DFS,재귀) -> 순열이용해서 풀이

--- 프로그래머스/Lv.2/87946. 피로도/피로도.py
+++ 프로그래머스/Lv.2/87946. 피로도/피로도.py
@@ -1,25 +1,18 @@
 import itertools
 
 def solution(k, dungeons):
-    # 모든 던전의 순서쌍을 생성
     orders = list(itertools.permutations(dungeons))
-
     max_dungeons = 0
-    # 모든 순서쌍에 대해
+    
     for order in orders:
         energy = k
         count = 0
-        # 각 순서에 따라 던전을 탐험
         for dungeon in order:
-            if energy >= dungeon[0]:  # 현재 피로도가 해당 던전의 최소 필요 피로도 이상이면
-                energy -= dungeon[1]  # 해당 던전을 탐험하고 피로도를 갱신
-                count += 1  # 탐험한 던전 수를 증가
+            if energy >= dungeon[0]:
+                energy -= dungeon[1] 
+                count += 1 
             else:
-                break  # 현재 피로도가 부족하면 더 이상 탐험할 수 없으므로 반복문을 종료
-        max_dungeons = max(max_dungeons, count)  # 탐험한 던전 수가 현재 최대치보다 많으면 갱신
+                break 
+        max_dungeons = max(max_dungeons, count) 
 
-    return max_dungeons  # 모든 경우를 탐색한 후, 탐험한 던전 수가 가장 많은 경우를 반환
-
-print(solution(80, [[80,20],[50,40],[30,10]])) # 3
-print(solution(100, [[100,10],[100,10],[100,10]])) # 1
-print(solution(100, [[30,10],[30,10],[30,10]])) # 3
+    return max_dungeons
