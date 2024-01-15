50199eef167b24279bc3e1d81a4f5dc4e84451e6
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-15 21:21:19 +0900

    ref: is_prime을 외웠으면 에라토스테네스의 체를 외우자

--- 프로그래머스/Lv.1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/Lv.1/12921. 소수 찾기/소수 찾기.py
@@ -12,3 +12,13 @@ def is_prime(num):
         if num%i == 0:
             return False
     return True
+
+# 다른 사람 풀이
+def solution(n):
+    num = set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num -= set(range(2*i,n+1,i))
+            
+    return len(num)
