649b38b6b84aebca139b5398a160e9686d7d5eb0
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-16 05:05:59 +0900

    ref: re.fullmatch(), re.search()
    - Try to apply the pattern to all of the string, returning a Match object, or None if no match was found.
    
    (function) def fullmatch(
        pattern: str | Pattern[str],
        string: str,
        flags: _FlagsType = 0
    ) -> (Match[str] | None)
    
    - Scan through string looking for a match to the pattern, returning a Match object, or None if no match was found.
    
    (function) def search(
        pattern: str | Pattern[str],
        string: str,
        flags: _FlagsType = 0
    ) -> (Match[str] | None)

--- 프로그래머스/Lv.1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/Lv.1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -4,6 +4,7 @@ def solution(babbling):
     answer = 0
     
     for char in babbling:
+        # 정규식을 이용하여 문자열이 aya, ye, woo, ma로만 이루어져 있고, aya, ye, woo, ma가 연속으로 나오지 않는지 확인
         if re.fullmatch(r"(aya|ye|woo|ma)+", char) and not re.search(r"(aya){2,}|(ye){2,}|(woo){2,}|(ma){2,}", char):
             answer += 1
             
