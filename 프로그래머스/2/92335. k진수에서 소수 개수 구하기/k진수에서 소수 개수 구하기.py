5c34f9f5ed5a261884834aaedd15b612fd279253
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-18 20:26:30 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,27 @@
+def solution(n, k):
+    # 주어진 숫자를 k진수로 변환
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+    
+    # n을 k진수로 변환
+    converted = ''
+    while n > 0:
+        n, mod = divmod(n, k) # 몫, 나머지
+        converted = str(mod) + converted # 나머지를 앞에 붙임
+        print(n, mod, converted)
+        
+    # k진수로 변환된 숫자를 0을 기준으로 split
+    candidates = converted.split('0')
+
+    # 소수 판별
+    count = 0
+    for candidate in candidates:
+        if candidate and is_prime(int(candidate)):
+            count += 1
+
+    return count
