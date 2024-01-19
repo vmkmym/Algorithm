a59b260366a7a0e8e84fee12fd37e8990b086a67
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-19 20:40:45 +0900

    ref: 완전탐색(itertools의 product), 리스트컴프리헨션 이중 for문, sum으로 가중치 구하기

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -7,3 +7,18 @@ def solution(word):
         answer += alpha[word[i]] * weight[i] + 1
 
     return answer
+
+# 완전탐색문제지만 완전탐색은 비효율적이므로 가중치를 이용해서 풀었음
+# 완전탐색은 product를 이용해서 풀었음
+from itertools import product
+
+def solution(word):
+    alphabet = ['A', 'E', 'I', 'O', 'U']
+    dictionary = []
+
+    for i in range(1, 6):
+        for combination in product(alphabet, repeat=i):
+            dictionary.append(''.join(combination))
+
+    dictionary.sort()
+    return dictionary.index(word) + 1
