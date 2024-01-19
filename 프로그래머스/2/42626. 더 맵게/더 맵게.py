53916eb6224d45d73c79229c619e01a7ec231fe3
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-19 21:43:24 +0900

    ref: heapq.heapify, heapq.heappop, heapq.heappush

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -7,7 +7,7 @@ def solution(scoville, K):
     for i in range(len(scoville)):
         if scoville[0] >= K:
             break
-        if len(scoville) == 1:
+        if len(scoville) == 1: # 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return
             return -1
         new = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
         heapq.heappush(scoville, new)
