e24ecd3b3c7e65fdf6108cb902274256b955c4d1
상은 <71699054+vmkmym@users.noreply.github.com>
2024-01-21 17:26:52 +0900

    ref: stack

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -10,3 +10,20 @@ def solution(prices):
         answer.append(count)
     
     return answer
+
+# 스택을 이용한 풀이
+def solution(prices):
+    answer = [0] * len(prices)
+    stack = []
+
+    for i, price in enumerate(prices):
+        while stack and price < prices[stack[-1]]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+
+    while stack:
+        j = stack.pop()
+        answer[j] = len(prices) - 1 - j
+        
+    return answer
