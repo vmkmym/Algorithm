5c31861dd009dde6d3d26d4837eea942900501af
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-01-31 23:41:20 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,7 +1,4 @@
 def solution(s, skip, index):
     alphabet = [chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in skip]
-    cipher_text = ''
-    for char in s:
-        char_index = (alphabet.index(char) + index) % len(alphabet)
-        cipher_text += alphabet[char_index]
+    cipher_text = ''.join([alphabet[(alphabet.index(char) + index) % len(alphabet)] for char in s])
     return cipher_text
