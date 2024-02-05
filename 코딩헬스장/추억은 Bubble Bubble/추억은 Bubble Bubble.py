b189945405fa59b25821e397beb809a45881dcf3
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-02-05 23:44:00 +0900

    추억은 Bubble Bubble.py 업데이트

--- 코딩헬스장/추억은 Bubble Bubble/추억은 Bubble Bubble.py
+++ 코딩헬스장/추억은 Bubble Bubble/추억은 Bubble Bubble.py
@@ -1,9 +1,9 @@
 # 버블 정렬 구현하기
 def bubble_sort(arr):
-    length = len(arr)-1
+    length = len(arr)-1 # 맨 마지막 요소는 제외
     for i in range(length):
         swap = False
-        for j in range(length-i):
+        for j in range(length-i): # i까지는 제외하고 크기 비교
             if arr[j] > arr[j+1]: 
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swap = True
