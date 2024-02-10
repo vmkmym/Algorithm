6737cecd0ae6fa72820b8bf5bd3ca4ffc5bb4b62
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-11 00:45:34 +0900

    ref: is_prime, set, from itertools import permutations, 순열은 tuple 반환

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -22,3 +22,17 @@ def solution(numbers):
             prime_count += 1
 
     return prime_count
+
+# 다른 사람의 풀이
+from itertools import permutations
+
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1)))) 
+        
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+        
+    return len(a)
