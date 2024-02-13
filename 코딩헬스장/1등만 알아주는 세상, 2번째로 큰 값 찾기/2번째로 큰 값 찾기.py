337db532967a9b527db880001996746b0687f501
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-14 00:02:01 +0900

    ref: Add test cases for solution function

--- 코딩헬스장/1등만 알아주는 세상, 2번째로 큰 값 찾기/2번째로 큰 값 찾기.py
+++ 코딩헬스장/1등만 알아주는 세상, 2번째로 큰 값 찾기/2번째로 큰 값 찾기.py
@@ -1,19 +1,24 @@
 def solution(numbers):
+    if len(numbers) < 2:
+        return "Error: 주어진 정수배열의 길이가 2보다 작습니다."
+    
     firstNum, secondNum = float('-inf'), float('-inf')
-    for num in set(numbers):
+    
+    for num in numbers:
         if num > firstNum:
             firstNum, secondNum = num, firstNum
-        elif num > secondNum:
+        elif num < firstNum and num > secondNum:
             secondNum = num
-    return secondNum
+            
+    return secondNum if secondNum != float('-inf') else "Error: 두 번째로 큰 수가 존재하지 않습니다."
 
 def test_solution():
     # 테스트 케이스
     assert solution([5,1,52,57,13,53]) == 53
     # 모든 원소가 같은 경우
-    assert solution([5, 5, 5, 5, 5]) == 5 
+    assert solution([5, 5, 5, 5, 5]) == "Error: 두 번째로 큰 수가 존재하지 않습니다."
     # 원소가 하나인 경우
-    assert solution([1]) == float('-inf')
+    assert solution([1]) == "Error: 주어진 정수배열의 길이가 2보다 작습니다."
     # 원소가 두 개인 경우
     assert solution([3, 3, 2, 2, 1, 1]) == 2
     # 음수만 존재하는 경우
