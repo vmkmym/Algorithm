a03757a9ec9119f447ccf72b33d6fd88152b3b89
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-02-22 00:38:30 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,11 @@
+def solution(ingredient):
+    burger = 0
+    i = 0
+    while i < len(ingredient) - 3:
+        if ingredient[i:i+4] == [1, 2, 3, 1]:
+            burger += 1
+            del ingredient[i:i+4]
+            i = max(0, i-4)
+        else:
+            i += 1
+    return burger
