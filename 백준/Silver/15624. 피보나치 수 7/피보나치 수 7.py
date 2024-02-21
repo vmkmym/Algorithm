901e4e9529f0bfb5fc81e860604a877c764c1660
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-02-22 01:11:58 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 216 ms, Memory: 70676 KB, Score: 201 point -BaekjoonHub

---
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+def fibonacci(n):
+    fib = [0, 1] + [0]*(n-1)
+    for i in range(2, n+1):
+        fib[i] = (fib[i-1] + fib[i-2]) % 1000000007
+    return fib[n]
+
+print(fibonacci(n))
