246d9afb903451420dc4c77f4516ff3b639619f0
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-22 00:49:28 +0900

    ref: stack 시간복잡도

--- 연습.py
+++ 연습.py
@@ -1,11 +1,11 @@
 def solution(ingredient):
     burger = 0
-    stack = []
-    for i in ingredient:
-        stack.append(i)
-        if stack[-4:] == [1, 2, 3, 1]:
+    i = 0
+    while i < len(ingredient) - 3:
+        if ingredient[i:i+4] == [1, 2, 3, 1]:
             burger += 1
-            stack = stack[:-4]
+            del ingredient[i:i+4]
+            i = max(0, i-4)  # 인덱스 값을 감소시킵니다.
+        else:
+            i += 1
     return burger
-
-# 5, 12번 시간초과
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,3 +1,4 @@
+# 인덱스를 이용한 풀이
 def solution(ingredient):
     burger = 0
     i = 0
@@ -6,6 +7,34 @@ def solution(ingredient):
             burger += 1
             del ingredient[i:i+4]
             i = max(0, i-4)
-        else:
+        else: 
             i += 1
     return burger
+
+# 다른 사람 풀이
+def solution(ingredient):
+    stack = []
+    burger = 0
+    for i in ingredient:
+        stack.append(i)
+        if stack[-4:] == [1, 2, 3, 1]:
+            burger += 1
+            # del이나 clear, slice를 사용하지 않고 pop을 사용하여 효율적으로 제거
+            # 처음에 이 풀이를 생각했는데, pop으로 안지워서 시간초과 발생했음
+            for i in range(4):
+                stack.pop()
+    return burger
+
+# 다른 사람 풀이 -> 제일 잘 푼 듯
+def solution(ingredient):
+    s = []
+    answer = 0
+    for i in ingredient:
+        s.append(i)
+        while s[-4:] == [1, 2, 3, 1]:
+            s.pop()
+            s.pop()
+            s.pop()
+            s.pop()
+            answer += 1
+    return answer
