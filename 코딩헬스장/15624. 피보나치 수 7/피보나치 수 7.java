f40e963acd24fa37515df147ffeaf36e03a8d643
상은 <71699054+vmkmym@users.noreply.github.com>
2024-02-22 19:13:08 +0900

    ref: 피보나치 수

--- 코딩헬스장/15624. 피보나치 수 7/피보나치 수 7.java
+++ 코딩헬스장/15624. 피보나치 수 7/피보나치 수 7.java
@@ -20,3 +20,29 @@ public class Main {
         return fib[n];
     }
 }
+
+// 메모리, 시간 개선 코드
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    System.out.println(fibonacci(n)); 
+  }
+
+  public static long fibonacci(int n) {
+    if (n < 0) return 0;
+    
+    long first = 0; 
+    long second = 1;
+    for (int i = 1; i <= n; i++) {
+      long temp = (first + second) % 1000000007;
+      first = second;
+      second = temp;
+    }
+    return first;
+  }
+
+}
