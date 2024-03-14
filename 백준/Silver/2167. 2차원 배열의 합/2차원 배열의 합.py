0885a03444ef0d480225fb539a347e81e459385d
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-03-14 21:28:57 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 100 ms, Memory: 37172 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,15 +1,20 @@
 import sys
 
+def calculate_cumulative_sum(arr, n, m):
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+    return dp
+
+def calculate_query(dp, i, j, x, y):
+    return dp[x][y] - dp[i-1][y] - dp[x][j-1] + dp[i-1][j-1]
+
 n, m = map(int, sys.stdin.readline().split())
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)] # 기준 배열
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 k = int(sys.stdin.readline())
+dp = calculate_cumulative_sum(arr, n, m)
 
-dp = [[0] * (m + 1) for _ in range(n + 1)]
-
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        dp[i][j] = arr[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] 
-        
 for _ in range(k):
     i, j, x, y = map(int, sys.stdin.readline().split())
-    print(dp[i-1][j-1] - dp[i-1][y] - dp[x][j-1] + dp[x][y])
+    print(calculate_query(dp, i, j, x, y))
