4e725ac854f8517a5168765ab9c60a57b7f819e5
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-03 22:13:55 +0900

    ref: 복습

--- 연습.py
+++ 연습.py
@@ -1,59 +1,32 @@
-'''
-    # 0: 청소 안한 곳, 1: 벽, 2: 청소한 곳
-    1. 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
-    2. if 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우,
-        2-1. 바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로 돌아간다.
-        2-2. 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
-    3. else 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우,
-        1. 반시계 방향으로 90도 회전한다.   
-        2. if 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 
-            2-1. 한 칸 전진한다.
-    3. 1번으로 돌아간다.
-'''
-
-# 현재 칸이 0이면 청소한다 room[x][y] = 2
-
-# 현재 칸 주변에 청소할 빈 칸이 없는 경우
-    # 후진할 수 있는지 확인
-        # 후진할 수 있다면 후진
-    # 후진할 수 없는 경우
-        # 작동을 멈춤
-
-# 현재 칸 주변에 청소할 빈 칸이 있는 경우
-    # (for문) 반시계 방향으로 90도 회전 for _ in range(3)
-    # if 칸이 청소할 빈 칸인지 확인
-        # 청소할 빈 칸이라면 전진
-            # 청소한 칸의 수를 세는 변수에 1을 더함
-        # 청소할 빈 칸이 아니라면 1번으로 돌아감
-# return cnt
-
-n, m = map(int, input().split())
-x, y, d = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(n)] 
-
-def turn_left(d): # 반시계 방향으로 90도 회전
-    return (d - 1) % 4
-
-def clean_room(x, y, d, room):
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
+def find_common_ancestor(tree, node1, node2):
+    ancestors = set()
+    while node1:
+        ancestors.add(node1)
+        node1 = tree.get(node1)
     
-    room[x][y] = 2 # 청소한 경우는 2로 표시
-    cnt = 1  # 무조건 처음엔 빈 칸이라 청소하니까
-    for _ in range(4):
-        d = turn_left(d)
-        nx = x + dx[d] 
-        ny = y + dy[d] 
-        if room[nx][ny] == 0:
-            cnt += clean_room(nx, ny, d, room)  # 청소한 칸의 수를 더함
-            return cnt  # 청소한 칸의 수를 반환
+    while node2:
+        if node2 in ancestors:
+            return node2
+        node2 = tree.get(node2)
+    
+    return None
 
-    # 모든 방향을 확인한 후에 후진하거나 작동을 멈추는 코드
-    nx = x - dx[d]
-    ny = y - dy[d]
-    if room[nx][ny] == 1:
-        return cnt  # 청소한 칸의 수를 반환
-    else:
-        return clean_room(nx, ny, d, room)  # 후진하지만 카운트는 증가시키지 않음
+# edges 간선을 이용하여 tree 구조를 만든다.
+def build_parents(edges):
+    tree = {}
+    for parent, child in edges:
+        tree[child] = parent
+    return tree
 
-print(clean_room(x, y, d, room))
+# 가장 가까운 공통 조상 찾기 
+def nearest_common_ancestor(edges, node1, node2):
+    tree = build_parents(edges)
+    return find_common_ancestor(tree, node1, node2)
+
+# 입력 받기
+test_cases = int(input())
+for _ in range(test_cases):
+    node_count = int(input())
+    edges = [list(map(int, input().split())) for _ in range(node_count - 1)]
+    node1, node2 = map(int, input().split())
+    print(nearest_common_ancestor(edges, node1, node2))
--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -20,34 +20,50 @@ def solution(survey, choices):
     answer = []
     for case in kakao_test:
         max_score = max(test.get(case[0], 0), test.get(case[1], 0))
-        for c in case:
+        # case: RT , max_score: 6
+        # case: CF , max_score: 0
+        # case: JM , max_score: 0
+        # case: AN , max_score: 0
+        
+        for c in case: # c가 없으면 기본값 0을 가져옴
             if test.get(c, 0) == max_score:
                 answer.append(c)
-                break
+                break # 그래서 둘 다 0인 경우는 첫번째 문자에서 break되기 때문에 앞에 있는 문자가 answer에 추가된다.
+                # c: R , test.get(c, 0): 6
+                # c: T , test.get(c, 0): 1
+                # c: C , test.get(c, 0): 0
+                # c: F , test.get(c, 0): 0
+                # c: J , test.get(c, 0): 0
+                # c: M , test.get(c, 0): 0
+                # c: A , test.get(c, 0): 0
+                # c: N , test.get(c, 0): 0
+                # answer:  ['R', 'C', 'J', 'A']
 
-    return "".join(answer)
+    return "".join(answer) 
 
-from collections import defaultdict
+print(solution(["TR", "RT", "TR"],[7, 1, 3])) # "RCJA"
 
-def solution(survey, choices):
-    kakao_test = ["RT", "CF", "JM", "AN"]
-    score = [0, 3, 2, 1, 0, 1, 2, 3]
-    test = defaultdict(int)
+# from collections import defaultdict
 
-    for i, j in zip(survey, choices):
-        diagree, agree = i[0], i[1]
+# def solution(survey, choices):
+#     kakao_test = ["RT", "CF", "JM", "AN"]
+#     score = [0, 3, 2, 1, 0, 1, 2, 3]
+#     test = defaultdict(int)
 
-        if j in [1, 2, 3]:
-            test[diagree] += score[j]
-        elif j in [5, 6, 7]:
-            test[agree] += score[j]
+#     for i, j in zip(survey, choices):
+#         diagree, agree = i[0], i[1]
 
-    answer = []
-    for case in kakao_test:
-        max_score = max(test[c] for c in case)
-        for c in case:
-            if test[c] == max_score:
-                answer.append(c)
-                break
+#         if j in [1, 2, 3]:
+#             test[diagree] += score[j]
+#         elif j in [5, 6, 7]:
+#             test[agree] += score[j]
 
-    return "".join(answer)
+#     answer = []
+#     for case in kakao_test:
+#         max_score = max(test[c] for c in case)
+#         for c in case:
+#             if test[c] == max_score:
+#                 answer.append(c)
+#                 break
+
+#     return "".join(answer)
