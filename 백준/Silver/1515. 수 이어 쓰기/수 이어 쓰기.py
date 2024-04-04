63b8376c7579547819f4b635114fc0419b8923a1
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-04 21:16:52 +0900

    style : 주어진 수 n, 숫자 number, 인덱스 idx
    - 숫자를 문자열로 변환하여 각 문자를 순회한다
    - 만약 주어진 수의 현재 인덱스가 현재 수 num이랑 같으면 idx를 증가하고 idx가 주어진 수의 길이보다 크거나 같으면 해당 수를 출력한다.
    - 그렇지 않으면 숫자를 하나씩 증가

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -1,14 +1,13 @@
 n = input()
 number = 0
-idx = 0
-
+idx = 0 
 while True:
-    number += 1
-    for num in str(number):
-        if n[idx] == num:
-            idx += 1
-            if idx >= len(n):
-                print(number)
+    number += 1 # 주어진 수의 현재 인덱스가 현재 수랑 일치하지 않으면 숫자를 늘린다
+    for num in str(number): 
+        if n[idx] == num: # 만약 주어진 수의 현재 인덱스가 현재 수랑 일치하면
+            idx += 1 # 인덱스 증가
+            if idx >= len(n): # 인덱스가 주어진 수의 길이보다 크거나 같으면
+                print(number) # 해당 수를 출력
                 break
     else:
         continue
