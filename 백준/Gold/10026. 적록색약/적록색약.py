58d1bd512968229b468b3198319f43bd75f5e5f1
SangEun <71699054+vmkmym@users.noreply.github.com>
2024-04-04 22:37:08 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -0,0 +1,45 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+array = [list(input().strip()) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+sum1 = 0
+sum2 = 0
+
+def dfs(x, y, color):
+    stack = [(x, y)]
+    while stack:
+        x, y = stack.pop()
+        visited[x][y] = 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == 0 and array[nx][ny] == color:
+                stack.append((nx, ny))
+
+for color in ['R', 'G', 'B']:
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == 0 and array[i][j] == color:
+                dfs(i, j, color)
+                sum1 += 1
+
+for i in range(n):
+    for j in range(n):
+        if array[i][j] == 'G':
+            array[i][j] = 'R'
+
+visited = [[0] * n for _ in range(n)]
+
+for color in ['R', 'B']:
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == 0 and array[i][j] == color:
+                dfs(i, j, color)
+                sum2 += 1
+
+print(sum1, sum2)
