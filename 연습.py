b4fc6ea24ae45ae5afb4ad8340bae0ab312730b5
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-08 23:37:28 +0900

    ref: dfs

--- 연습.py
+++ 연습.py
@@ -1,32 +1,25 @@
-def find_common_ancestor(tree, node1, node2):
-    ancestors = set()
-    while node1:
-        ancestors.add(node1)
-        node1 = tree.get(node1)
-    
-    while node2:
-        if node2 in ancestors:
-            return node2
-        node2 = tree.get(node2)
-    
-    return None
-
-# edges 간선을 이용하여 tree 구조를 만든다.
-def build_parents(edges):
-    tree = {}
-    for parent, child in edges:
-        tree[child] = parent
-    return tree
-
-# 가장 가까운 공통 조상 찾기 
-def nearest_common_ancestor(edges, node1, node2):
-    tree = build_parents(edges)
-    return find_common_ancestor(tree, node1, node2)
-
-# 입력 받기
-test_cases = int(input())
-for _ in range(test_cases):
-    node_count = int(input())
-    edges = [list(map(int, input().split())) for _ in range(node_count - 1)]
-    node1, node2 = map(int, input().split())
-    print(nearest_common_ancestor(edges, node1, node2))
+def dfs(current_node, graph, visited):
+    cnt = 1
+    visited[current_node] = True
+    for adjacent_node in graph[current_node]:
+        if not visited[adjacent_node]:
+            cnt += dfs(adjacent_node, graph, visited)
+    return cnt
+        
+def solution(n, wires):
+    answer = float('inf')
+    graph = [[] for _ in range(n+1)]
+    for v1, v2 in wires:
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    for i in range(n-1):
+        visited = [False] * (n+1)
+        v1, v2 = wires[i]
+        graph[v1].remove(v2)
+        graph[v2].remove(v1)
+        cnt1 = dfs(v1, graph, visited)
+        cnt2 = n - cnt1
+        answer = min(answer, abs(cnt1 - cnt2))
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    return answer
