97921a7b527dae5c4bc758ff040e371d171aa9a4
상은 <71699054+vmkmym@users.noreply.github.com>
2024-04-30 23:31:21 +0900

    ref: 코드 복습

--- 연습.py
+++ 연습.py
@@ -1,37 +1,26 @@
-def solution(today, terms, privacies):
-    '''
-    today : 오늘 날짜
-    terms : ["약관타입 유효기간"]
-    privacies : ["수집일자 약관타입"]
-    => 파기해야 할 개인정보의 번호를 오름차순으로 1차원 정수 배열에 담아 return 
-    '''
-    term_validity = {}  # 약관별 유효 기간 저장
-    for term in terms:
-        term_name, duration = term.split()
-        term_validity[term_name] = int(duration) # 유효기간을 정수로 담기
-    
-    # 파기해야 할 개인정보의 번호를 담을 리스트
-    answer = []
-    
-    for idx, privacy in enumerate(privacies):
-        # 수집일자, 약관타입으로 분리
-        collection_date, term = privacy.split()
-        # 수집일자를 년, 월, 일로 분리해서 정수로 변환
-        year, month, day = map(int, collection_date.split('.'))
-        
-        # 약관 타입에 따른 유효기간을 가져옴
-        term_duration = term_validity[term]
-        
-        # 만료일 계산, 현재 연도 + (현재 월 + 유효기간 - 1) // 12, (현재 월 + 유효기간 - 1) % 12 + 1
-        # 1을 빼는 이유는 현재 월을 포함해서 계산하기 때문
-        expiry_year = year + (month + term_duration - 1) // 12
-        expiry_month = (month + term_duration - 1) % 12 + 1
-        
-        # YYYY.MM.DD 형식으로 만료일을 저장
-        expiry_date = f"{expiry_year:04d}.{expiry_month:02d}.{day:02d}"
-        
-        # 만약 만료일이 오늘 날짜보다 이전이라면 파기 리스트에 추가
-        if expiry_date <= today:
-            answer.append(idx + 1)  # 개인정보번호는 1부터 시작하므로 idx + 1 추가
-    
-    return answer
+n, m = map(int, input().split())
+board = [list(input()) for _ in range(n)]
+
+def min_paints(board, n, m):
+    result = [] 
+    for i in range(n-7): # 8x8 부분 체스판을 선택할 수 있는 모든 경우의 수
+        for j in range(m-7):
+            count1 = 0 # 첫 번째 칸이 W인 경우
+            count2 = 0 # 첫 번째 칸이 B인 경우
+            for k in range(i, i+8): # 선택된 8x8 부분 체스판 내의 각 칸을 순회
+                for l in range(j, j+8):
+                    if (k+l)%2 == 0: # 행과 열의 합이 짝수인 경우
+                        if board[k][l] != 'W': 
+                            count1 += 1
+                        if board[k][l] != 'B':
+                            count2 += 1
+                    else:
+                        if board[k][l] != 'B':
+                            count1 += 1
+                        if board[k][l] != 'W':
+                            count2 += 1
+            result.append(count1)
+            result.append(count2)
+    return min(result)
+
+print(min_paints(board, n, m))
