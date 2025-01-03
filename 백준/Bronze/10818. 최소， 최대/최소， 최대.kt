cda1475aeb89e05a63d54cade3f2a46a63536bc0
SangEun <71699054+vmkmym@users.noreply.github.com>
2025-01-03 17:14:28 +0900

    ref : 구현, 수학
    - 리스트를 한 번만 순회해서 풀기(최소값, 최대값으로 초기화하여 각 digit을 초기화된 값이랑 비교해서 갱신하면 한 번만 순회하여 효율적으로 풀 수 있음.

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
@@ -3,3 +3,26 @@ fun main() {
     val digits = readLine()!!.split(" ").map { it.toInt() }
     println("${digits.minOrNull()} ${digits.maxOrNull()}")
 }
+
+// 내 풀이 문제점
+// digits.minOrNull()와 digits.maxOrNull()를 각각 호출하여 리스트를 두 번 순회함
+// 시간복잡도가 O(2n)이므로 리스트를 한 번만 순회하는 O(n)보다 비효율적이다.
+// 두 번의 리스트 순회로 메모리 사용량도 증가한다.
+
+// 보완할 방법 : 리스트 한 번만 순회하기
+fun main() {
+    val n = readLine()!!.toInt()
+    val digits = readLine()!!.split(" ").map { it.toInt() }
+    
+    var min = Int.MAX_VALUE // Int.MAX_VALUE로 초기화
+    var max = Int.MIN_VALUE // Int.MIN_VALUE로 초기화
+    
+    for (digit in digits) {
+        if (digit < min) min = digit
+        if (digit > max) max = digit
+    }
+    
+    println("$min $max")
+}
+
+// 시간복잡도가 O(n)이므로 효율적이다. 
